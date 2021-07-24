3c0b3c3d64037d260b224ccec7965e381507a9e9
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2021-07-24 20:52:14 +0700

    Add files via upload

---
+++ rom/bffb.py
@@ -0,0 +1,34 @@
+# Hello broh
+import os,sys
+
+def menu():
+	os.system ("clear")
+	print ("\n \x1b[1;92mHi, Maaf alat ini telah di hapus ")
+	print (" Tunggu update terbaru nya :)")
+	print wa
+	print wa_lan
+	print (" Hi, Sorry this tool has been removed ")
+	print (" Wait for the latest update :) ")
+	print wa_
+	print wa__lan
+	rom = raw_input (" \x1b[1;95m# \x1b[1;96m? \x1b[1;91m> \x1b[1;93m")
+	if rom =='':
+		print ("\n\x1b[0;97m$ exit ")
+	elif rom in ['wa','WA','Wa']:
+		wa_me()
+	else:
+		print ("\n\x1b[0;97m$ exit ")
+	
+wa = (" Jika ingin di tanyakan bisa ke no wa saya ")
+wa_lan = (" Anda bisa ketik Wa + enter untuk menghungi saya \n")
+wa_ = (" If you want to ask, you can go to my wa number ")
+wa__lan = (" You can type Wa + enter to call me \n ")
+
+def wa_me():
+	os.system('am start https://wa.me/+6282371648186?text=Assalamualaikum+bang >/dev/null')
+	
+
+if __name__ == '__main__':
+	os.system ('git pull')
+	menu()
+	
